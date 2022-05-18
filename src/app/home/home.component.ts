d621fdf6a9eeb75b4a7bc2ff8cb201c575a956a7
Davide <d.sadotti@gmail.com>
2022-05-18 12:11:57 +0200

    Pagina home completata con poco css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,6 +18,53 @@ export class HomeComponent implements OnInit {
 
   products: any[] = [];
 
+  reviews: any[] = [
+    {
+      image: "./../../assets/reviewOne.jpg",
+      title: "Lorem Ipsum",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget tempus erat. Nullam elementum erat in velit porttitor, ut commodo nulla suscipit. Mauris diam nunc, ultrices et viverra id, consequat a dolor.",
+      date: "Maggio 10, 2022",
+      writer: "Nulla Aliquam"
+    },
+    {
+      image: "./../../assets/reviewTwo.jpg",
+      title: "Lorem Ipsum",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget tempus erat. Nullam elementum erat in velit porttitor, ut commodo nulla suscipit. Mauris diam nunc, ultrices et viverra id, consequat a dolor.",
+      date: "Maggio 18, 2022",
+      writer: "Nulla Aliquam"
+    },
+    {
+      image: "./../../assets/reviewThree.jpg",
+      title: "Lorem Ipsum",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget tempus erat. Nullam elementum erat in velit porttitor, ut commodo nulla suscipit. Mauris diam nunc, ultrices et viverra id, consequat a dolor.",
+      date: "Maggio 27, 2022",
+      writer: "Nulla Aliquam"
+    }
+  ]
+
+  services: any[] = [
+    {
+      icon: "./../../assets/plane.png",
+      title: "Free Shipping",
+      text: "Free shipping for all US order"
+    },
+    {
+      icon: "./../../assets/salvagente.png",
+      title: "Support 24/7",
+      text: "We support 24 hours a day"
+    },
+    {
+      icon: "./../../assets/reload.png",
+      title: "30 days return",
+      text: "You have 30 days to return"
+    },
+    {
+      icon: "./../../assets/scudo.png",
+      title: "Payment 100% Secure",
+      text: "Payment 100% Secure"
+    },
+  ]
+
   getProductsList(){
     this.productsListService.getList().subscribe((list: any) => {
       this.products = list.products;
